b167bf3587aa6c1462ef3a71f3220b1cdab9d89d
Vincent Servio <vincentservio@gmail.com>
2020-01-27 20:58:20 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,6 +8,10 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
+      begin
+        raise YourCustomError
+        rescue YourCustomError
+      end
     raise PartnerError
   else 
     person.partner = self 
